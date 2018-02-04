aa59b831ca572cc2b85f34b3dbf93fe27b555964
Fioletibiel <pawel89kaminski@gmail.com>
2018-02-04 18:52:47 +0100

    Initial commit

---
+++ app/src/main/java/widget/miniprojekt5/lab6/pawelkaminski/pjwstk/edu/pl/widget/GlownaAktywnosc.java
@@ -0,0 +1,13 @@
+package widget.miniprojekt5.lab6.pawelkaminski.pjwstk.edu.pl.widget;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GlownaAktywnosc extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aktywnosc_glowna);
+    }
+}
